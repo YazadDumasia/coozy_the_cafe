02cb4f8c88acb2577e048e49ea30cb7e5af8aaf3
Yazad Dumasia <yazad14015dumasia+github@gmail.com>
2026-08-08 18:38:37 +0530

    refactor: remove unused general bloc and cubit presentation files

--- lib/packages/table_info/presentation/bloc/cubit/general_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'general_state.dart';
-
-class GeneralCubit extends Cubit<GeneralState> {
-  GeneralCubit() : super(GeneralInitial());
-}
--- lib/packages/table_info/presentation/bloc/generalbloc_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'generalbloc_bloc.dart';
-
-sealed class GeneralblocEvent extends Equatable {
-  const GeneralblocEvent();
-
-  @override
-  List<Object> get props => [];
-}
